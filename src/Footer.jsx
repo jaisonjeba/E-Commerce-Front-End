361d380e831fd197a0579e0220ffdbd9d9954e65
jaisonjeba <jaiprabakar5@gmail.com>
2024-12-11 10:50:28 +0530

    cart added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,8 @@
 import "./Footer.css";
 
 export const Footer = () => {
-  const linkedIn = "https://www.linkedin.com/in/ajithkumar-m-2603b5175/";
-  const gitHub = "https://github.com/AJITH1509";
+  const linkedIn = "https://www.linkedin.com/in/jaison-jeba-8328b6195/";
+  const gitHub = "https://github.com/jaisonjeba";
 
   const handleClick = (link) => {
     window.open(link);
@@ -10,20 +10,20 @@ export const Footer = () => {
 
   return (
     <div className="footer">
-      <p>©️ reserved by Ajith Kumar</p>
+      <p>©️ reserved by Jaison</p>
       <div className="social">
         <img
           onClick={() => handleClick(linkedIn)}
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
           alt="linkedin"
         />
-        <label>AjithKumar M</label>
+        <label>Jaison Jeba</label>
         <img
           onClick={() => handleClick(gitHub)}
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
           alt="github"
         />
-        <label>AJITH1509</label>
+        <label>Jaison Jeba</label>
       </div>
     </div>
   );
